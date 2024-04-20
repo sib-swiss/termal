ee879455fd0f2c9f8a83e520022f0b0798082170
Thomas Junier <thomas.junier@sib.swiss>
2024-04-20 14:45:59 +0200

    Stage 1 ok

---
+++ roadmap.md
@@ -0,0 +1,4 @@
+Termal
+======
+
+1. [ ] Display a rectangular array of characters at the top left corner of the screen.
---
+++ src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    print!("{esc}[2J{esc}[1;1H", esc = 27 as char);
+    println!("ATGCCGTAGTGAA");
+    println!("ATGCCGTAGTGAA");
+    println!("ATGCCGTAGTGAA");
+    println!("ATGCCGTAGTGAA");
+    println!("ATGCCGTAGTGAA");
+    println!("ATGCCGTAGTGAA");
+}
