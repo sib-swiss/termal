2294fff58ac24088213777944a9bd0eba1fc8678
Thomas Junier <thomas.junier@sib.swiss>
2024-06-08 15:46:29 +0200

    Test script now handles errors

--- app-tests/run-tests.sh
+++ app-tests/run-tests.sh
@@ -1,33 +1,38 @@
 #!/usr/bin/env bash
 
-set -ue
+# Don't set -e, as we need to report scripts that fail.
+#
+set -u
 
-source ~/bin/functional-bash.sh || {
-	printf "Could not find functional-bash.sh - aborting." >&2
+source ~/bin/bash-warn.sh || {
+	printf "Could not source functional-bash.sh - aborting." >&2
 	exit 1
 }
+
+source ~/bin/functional-bash.sh || die "Could not source ~/bin/functional-bash.sh"
 
 
 # Converts Unix exit status to label ("success" or "failure")
 #
 status_to_label() {
     local -ri status=$1
-    if ((0 == status)); then 
-        echo success
-    else
-        echo failure
-    fi
+     # We treat 126 and 127 (which indicate permission error and file not found,
+     # respectively) as indications of error rather than failure.
+    case $status in
+        0 ) echo success ;;
+        126 | 127 ) echo error ;;
+        * ) echo failure ;;
+    esac
 }
 
-# Converts Unix exit status to boolean (0 / !=0 -> 1 / 0 resp)
-#
-status_to_boolean() {
-    local -ri status=$1
-    if ((0 == status)); then 
-        echo 1
-    else
-        echo 0
-    fi
+colour_label() {
+    local -r label=$1
+    case $label in 
+        success ) printf "%s" "$(green "$label")" ;;
+        failure ) printf "%s" "$(red "$label")" ;;
+        error   ) printf "%s" "$(yellow "$label")" ;;
+        *       ) die "unknown status label '$label'"
+    esac
 }
 
 ################################################################
@@ -52,19 +57,28 @@ done
 
 declare -A test_result_labels
 aamap status_to_label test_status test_result_labels
-#declare -p test_result_labels
 
 echo
 for PID in "${!test_names[@]}"; do
     printf "%s -> %s\n" "${test_names["$PID"]}" \
-        "${test_result_labels["$PID"]}"
+        "$(colour_label "${test_result_labels["$PID"]}")"
 done
 
-declare -A test_result_bools
-aamap status_to_boolean test_status test_result_bools
-#declare -p test_result_bools
+declare -A test_result_counts
+count test_result_labels test_result_counts
 
-declare -i nb_successes="$(sum test_result_bools)"
-declare -i nb_failures=$((${#test_names[@]} - nb_successes))
-printf "%d success(es), %d failure(s)\n" "$nb_successes" \
-    "$nb_failures"
+[[ -v test_result_counts[success] ]] && nb_successes="${test_result_counts[success]}" || nb_successes=0
+[[ -v test_result_counts[failure] ]] && nb_failures="${test_result_counts[failure]}"  || nb_failures=0
+[[ -v test_result_counts[error] ]]   && nb_errors="${test_result_counts[error]}"      || nb_errors=0
+
+printf "\n%s:\t%d\n%s:\t%d\n%s:\t\t%d\n" \
+    "$(green Successes)" "$nb_successes" \
+    "$(red Failures)" "$nb_failures" \
+    "$(yellow Errors)" "$nb_errors" \
+
+printf "\n"
+if ((${#test_names[@]} == $nb_successes)) ; then
+    printf "All %d tests %s!\n" "$nb_successes" "$(green OK)"
+else
+    printf "There were %s or %s.\n" "$(yellow errors)" "$(red failures)"
+fi
