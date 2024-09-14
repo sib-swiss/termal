a325dbdfe7b60c70cf8512196bd32f7534af020c
Thomas Junier <thomas.junier@sib.swiss>
2024-09-14 14:51:48 +0200

    option and key (v) to set zoom box guides

--- src/ui.rs
+++ src/ui.rs
@@ -410,6 +410,10 @@ impl<'a> UI<'a> {
         }
     }
 
+    pub fn set_zoombox_guides(&mut self, state: bool) {
+        self.show_zb_guides = state;
+    }
+
     pub fn toggle_hl_retained_cols(&mut self) {
         self.highlight_retained_cols = !self.highlight_retained_cols;
     }
