30b56531e9001703e4d4fdd1ec7873de1fbf93a0
Thomas Junier <thomas.junier@sib.swiss>
2025-01-14 11:23:16 +0100

    + ClustalX colors

--- src/ui/color_scheme.rs
+++ src/ui/color_scheme.rs
@@ -5,9 +5,20 @@ use ratatui::prelude::Color;
 use crate::ui::color_map::ColorMap;
 use crate::ui::color_map_lesk;
 
+// In-house colors
 pub const ORANGE: Color = Color::Rgb(255, 165, 0);
 pub const SALMON: Color = Color::Rgb(250, 128, 114);
-//pub const DARK_GREEN: Color = Color::Rgb(
+
+// ClustalX colors (source:
+// https://www.cgl.ucsf.edu/chimera/1.2065/docs/ContributedSoftware/multalignviewer/colprot.par)
+pub const CLUSTALX_RED: Color = Color::Rgb(229, 51, 25);
+pub const CLUSTALX_BLUE: Color = Color::Rgb(25, 127, 229);
+pub const CLUSTALX_GREEN: Color = Color::Rgb(25, 204, 25);
+pub const CLUSTALX_CYAN: Color = Color::Rgb(25, 178, 178);
+pub const CLUSTALX_PINK: Color = Color::Rgb(229, 127, 127);
+pub const CLUSTALX_MAGENTA: Color = Color::Rgb(204, 76, 204);
+pub const CLUSTALX_YELLOW: Color = Color::Rgb(204, 204, 0);
+pub const CLUSTALX_ORANGE: Color = Color::Rgb(229, 153, 76);
 
 pub struct ColorScheme {
     pub label_num_color: Color,
