40e22689da39abe1d73a0491a0a08416aa0b306e
Thomas Junier <thomas.junier@sib.swiss>
2026-02-01 22:05:20 +0100

    more tspans

--- crates/termal-export/src/svg.rs
+++ crates/termal-export/src/svg.rs
@@ -1,5 +1,7 @@
 use std::io::{Result, Write};
 
+use itertools::Itertools;
+
 use termal_alignment::Alignment;
 
 use crate::ExportOpts;
@@ -14,21 +16,27 @@ pub fn export_svg<W: Write>(aln: &Alignment, opts: &ExportOpts, out: &mut W) -> 
 }
 
 fn svg_open(width: f32, height: f32) -> String {
-    format!(r#"<?xml version="1.0" encoding="UTF-8"?>
-<svg xmlns="http://www.w3.org/2000/svg" width="{}" height="{}">"#, width, height)
+    format!("<?xml version='1.0' encoding='UTF-8'?>
+<svg xmlns='http://www.w3.org/2000/svg' width='{}' height='{}'>", width, height)
 }
 
 fn svg_aln_cell(c: char, x: f32, y: f32) -> String {
-    format!(r#"<text x="{}" y="{}">{}</text>"#, x, y, c)
+    format!("<text x='{}' y='{}'>{}</text>", x, y, c)
 }
 
 fn svg_sequence(seq: &str, x: f32, y: f32) -> String {
-    format!(r#"<text font-family="mono" x="{}" y="{}">{}</text>"#, x, y, seq)
+    let hdr = "<text font-family='mono'>";
+    let cells = seq.chars().enumerate().map(|(i, c)| 
+        format!("<tspan x='{}' y='20'>{}</tspan>", (i as f32 * 10.0), c)
+        ).join("");
+    format!("{}{}</text>", hdr, cells)
 }
 
 fn write_aln<W: Write>(aln: &Alignment, opts: &ExportOpts, out: &mut W) -> Result<()> {
-    writeln!(out, r#"<text x="0", y="0" font-family="mono">"#)?;
-    writeln!(out, "</text>")?;
+    //writeln!(out, r#"<text x="0" y="0" font-family="mono">"#)?; // TODO: make a group, not a text
+
+    writeln!(out, "{}", svg_sequence("GAATTC", 10.0, 20.0));
+    //writeln!(out, "</text>")?;
     Ok(())
 }
 
