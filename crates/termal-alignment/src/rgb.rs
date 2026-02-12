a8bd03cc0e46b0b2e042c81c06c4af2a8347255d
Thomas Junier <thomas.junier@sib.swiss>
2026-02-12 17:56:39 +0100

    all tests ok

--- crates/termal-alignment/src/rgb.rs
+++ crates/termal-alignment/src/rgb.rs
@@ -1,8 +1,8 @@
 #[derive(Clone, Copy, Debug, PartialEq)]
 pub struct Rgb {
-    r: u8,
-    g: u8,
-    b: u8,
+    pub r: u8,
+    pub g: u8,
+    pub b: u8,
 }
 
 impl Rgb {
@@ -57,17 +57,30 @@ impl Rgb {
     }
 }
 
-const RGB_RED: Rgb = Rgb{r: 255, g: 0, b: 0};
-const RGB_GRAY: Rgb = Rgb{r: 127, g: 127, b: 127};
-const RGB_WHITE: Rgb = Rgb{r: 255, g: 255, b: 255};
+pub const RGB_RED: Rgb = Rgb{r: 255, g: 0, b: 0};
+pub const RGB_GRAY: Rgb = Rgb{r: 127, g: 127, b: 127};
+pub const RGB_WHITE: Rgb = Rgb{r: 255, g: 255, b: 255};
 
-const GAP_COLOR: Rgb = RGB_GRAY;
+pub const GAP_COLOR: Rgb = RGB_GRAY;
 
 // In-house colors
-pub const ORANGE: Rgb = Rgb{r: 255, g: 165, b: 0};
-pub const SALMON: Rgb = Rgb{r: 250, g: 128, b: 114};
+pub const TERMAL_ORANGE: Rgb  = Rgb {r: 255, g: 165, b: 0};
+pub const TERMAL_SALMON: Rgb  = Rgb {r: 250, g: 128, b: 114};
 
-// ClustalX colors (source:
+// ASCII 8-Color palette colors
+pub const ASCII_8_COLOR_GREEN: Rgb   = Rgb { r: 0,   g: 128, b: 0   };
+pub const ASCII_8_COLOR_MAGENTA: Rgb = Rgb { r: 128, g: 0,   b: 128 };
+pub const ASCII_8_COLOR_RED: Rgb     = Rgb { r: 128, g: 0,   b: 0   };
+pub const ASCII_8_COLOR_BLUE: Rgb    = Rgb { r: 0,   g: 0,   b: 128 };
+
+// Lesk aa colors (source?)
+pub const LESK_ORANGE: Rgb = TERMAL_ORANGE;
+pub const LESK_GREEN: Rgb = ASCII_8_COLOR_GREEN;
+pub const LESK_BLUE: Rgb = ASCII_8_COLOR_BLUE;
+pub const LESK_MAGENTA: Rgb = ASCII_8_COLOR_MAGENTA;
+pub const LESK_RED: Rgb = ASCII_8_COLOR_RED;
+
+// ClustalX aa colors (source:
 // https://www.cgl.ucsf.edu/chimera/1.2065/docs/ContributedSoftware/multalignviewer/colprot.par)
 pub const CLUSTALX_RED: Rgb = Rgb{r: 229, g: 51, b: 25};
 pub const CLUSTALX_BLUE: Rgb = Rgb{r: 25, g: 127, b: 229};
@@ -101,6 +114,7 @@ pub const JALVIEW_NUCLEOTIDE_V: Rgb = Rgb::from_u32(0x00b8860b);
 pub const JALVIEW_NUCLEOTIDE_N: Rgb = Rgb::from_u32(0x002f4f4f);
 
 
+#[derive(Clone)]
 pub struct ResidueColorMap {
     table: [Rgb; 256],
 }
@@ -111,7 +125,7 @@ impl ResidueColorMap {
         self.table[b as usize]
     }
 
-    fn with_default(gap_color: Rgb, default: Rgb) -> Self {
+    pub fn with_default(gap_color: Rgb, default: Rgb) -> Self {
         let mut tbl = [default; 256];
         tbl[b'-' as usize] = gap_color;
         tbl[b'.' as usize] = gap_color;
@@ -122,9 +136,14 @@ impl ResidueColorMap {
         self.table[b as usize] = color;
     }
 
-    fn set_pair(&mut self, b: u8, color: Rgb) {
+    pub fn set_pair(&mut self, b: u8, color: Rgb) {
         self.set(b.to_ascii_lowercase(), color);
         self.set(b.to_ascii_uppercase(), color);
+    }
+
+
+    pub fn monochrome() -> Self {
+        Self::with_default(GAP_COLOR, RGB_WHITE)
     }
 
     pub fn dna_basic() -> Self {
@@ -136,7 +155,30 @@ impl ResidueColorMap {
         map
     }
 
-    pub fn dna_clustalx() -> Self {
+    pub fn dna_jalview() -> Self {
+        let mut map = Self::with_default(GAP_COLOR, RGB_WHITE);
+        map.set_pair(b'A', JALVIEW_NUCLEOTIDE_A);
+        map.set_pair(b'C', JALVIEW_NUCLEOTIDE_C);
+        map.set_pair(b'G', JALVIEW_NUCLEOTIDE_G);
+        map.set_pair(b'T', JALVIEW_NUCLEOTIDE_T);
+        map.set_pair(b'U', JALVIEW_NUCLEOTIDE_U);
+        map.set_pair(b'I', JALVIEW_NUCLEOTIDE_I);
+        map.set_pair(b'X', JALVIEW_NUCLEOTIDE_X);
+        map.set_pair(b'R', JALVIEW_NUCLEOTIDE_R);
+        map.set_pair(b'Y', JALVIEW_NUCLEOTIDE_Y);
+        map.set_pair(b'W', JALVIEW_NUCLEOTIDE_W);
+        map.set_pair(b'S', JALVIEW_NUCLEOTIDE_S);
+        map.set_pair(b'M', JALVIEW_NUCLEOTIDE_M);
+        map.set_pair(b'K', JALVIEW_NUCLEOTIDE_K);
+        map.set_pair(b'B', JALVIEW_NUCLEOTIDE_B);
+        map.set_pair(b'H', JALVIEW_NUCLEOTIDE_H);
+        map.set_pair(b'D', JALVIEW_NUCLEOTIDE_D);
+        map.set_pair(b'V', JALVIEW_NUCLEOTIDE_V);
+        map.set_pair(b'N', JALVIEW_NUCLEOTIDE_N);
+        map
+    }
+
+    pub fn aa_clustalx() -> Self {
         let mut map = Self::with_default(GAP_COLOR, RGB_WHITE);
         map.set_pair(b'G', CLUSTALX_ORANGE);
         map.set_pair(b'A', CLUSTALX_BLUE);
@@ -158,6 +200,32 @@ impl ResidueColorMap {
         map.set_pair(b'E', CLUSTALX_MAGENTA);
         map.set_pair(b'K', CLUSTALX_RED);
         map.set_pair(b'R', CLUSTALX_RED);
+        map
+    }
+
+    pub fn aa_lesk() -> Self {
+        let mut map = Self::with_default(GAP_COLOR, RGB_WHITE);
+        map.set_pair(b'G', TERMAL_ORANGE);
+        map.set_pair(b'A', TERMAL_ORANGE);
+        map.set_pair(b'S', TERMAL_ORANGE);
+        map.set_pair(b'T', TERMAL_ORANGE);
+        map.set_pair(b'C', ASCII_8_COLOR_GREEN);
+        map.set_pair(b'V', ASCII_8_COLOR_GREEN);
+        map.set_pair(b'I', ASCII_8_COLOR_GREEN);
+        map.set_pair(b'L', ASCII_8_COLOR_GREEN);
+        map.set_pair(b'P', ASCII_8_COLOR_GREEN);
+        map.set_pair(b'F', ASCII_8_COLOR_GREEN);
+        map.set_pair(b'Y', ASCII_8_COLOR_GREEN);
+        map.set_pair(b'M', ASCII_8_COLOR_GREEN);
+        map.set_pair(b'W', ASCII_8_COLOR_GREEN);
+        map.set_pair(b'N', ASCII_8_COLOR_MAGENTA);
+        map.set_pair(b'Q', ASCII_8_COLOR_MAGENTA);
+        map.set_pair(b'H', ASCII_8_COLOR_MAGENTA);
+        map.set_pair(b'D', ASCII_8_COLOR_RED);
+        map.set_pair(b'E', ASCII_8_COLOR_RED);
+        map.set_pair(b'K', ASCII_8_COLOR_BLUE);
+        map.set_pair(b'R', ASCII_8_COLOR_BLUE);
+        map.set_pair(b'X', RGB_WHITE);
         map
     }
 }
@@ -187,8 +255,8 @@ mod test {
     }
 
     #[test]
-    fn test_dna_clustalx() {
-        let cmap = ResidueColorMap::dna_clustalx();
+    fn test_aa_clustalx() {
+        let cmap = ResidueColorMap::aa_clustalx();
         assert_eq!(CLUSTALX_MAGENTA, cmap.rgb(b'D'));
         assert_eq!(CLUSTALX_MAGENTA, cmap.rgb(b'd'));
         assert_eq!(GAP_COLOR, cmap.rgb(b'-'));
--- crates/termal/src/ui/color_map.rs
+++ crates/termal/src/ui/color_map.rs
@@ -19,29 +19,24 @@ use crate::ui::color_scheme::{
     JALVIEW_NUCLEOTIDE_V, JALVIEW_NUCLEOTIDE_W, JALVIEW_NUCLEOTIDE_X, JALVIEW_NUCLEOTIDE_Y, ORANGE,
 };
 
+use termal_alignment::rgb::{
+    GAP_COLOR,
+    RGB_WHITE,
+    ResidueColorMap,
+    Rgb,
+};
+
 #[derive(Clone)]
 pub struct ColorMap {
-    #[allow(dead_code)]
     pub name: String,
-    map: HashMap<char, Color>,
+    map: ResidueColorMap,
 }
 
 impl ColorMap {
-    pub fn new(name: String, map: HashMap<char, Color>) -> ColorMap {
-        ColorMap { name, map }
-    }
 
     pub fn get(&self, residue: char) -> Color {
-        if let Some(color) = self.map.get(&residue) {
-            *color
-        } else {
-            Color::White
-        }
-    }
-
-    #[allow(dead_code)]
-    pub fn insert(&mut self, residue: char, color: Color) {
-        self.map.insert(residue, color);
+        let rgb = self.map.rgb(residue as u8);
+        Color::Rgb(rgb.r, rgb.g, rgb.b)
     }
 }
 
@@ -63,211 +58,32 @@ pub fn monochrome_colormap() -> Vec<ColorMap> {
     vec![color_map_monochrome()]
 }
 
-// NOTE: if it turns out that these hash maps are not efficient (didn't benchmark yet), we might
-// want to look at perfect hash functions - see e.g https://crates.io/crates/phf
-
-// NOTE, although these maps do not vary, we cannot dclare them as constants, because they involve
-// a function call (namely, to HashMap::from()).
-
-// It's prolly easier to have a no-op colorscheme than to decide at every iteration if we do a
-// lookup or not.
-
-pub fn color_map_monochrome() -> ColorMap {
-    ColorMap::new(
-        "White".into(),
-        HashMap::from([
-            ('G', Color::White),
-            ('A', Color::White),
-            ('S', Color::White),
-            ('T', Color::White),
-            ('C', Color::White),
-            ('V', Color::White),
-            ('I', Color::White),
-            ('L', Color::White),
-            ('P', Color::White),
-            ('F', Color::White),
-            ('Y', Color::White),
-            ('M', Color::White),
-            ('W', Color::White),
-            ('N', Color::White),
-            ('Q', Color::White),
-            ('H', Color::White),
-            ('D', Color::White),
-            ('E', Color::White),
-            ('K', Color::White),
-            ('R', Color::White),
-            ('X', Color::White),
-            ('g', Color::White),
-            ('a', Color::White),
-            ('s', Color::White),
-            ('t', Color::White),
-            ('c', Color::White),
-            ('v', Color::White),
-            ('i', Color::White),
-            ('l', Color::White),
-            ('p', Color::White),
-            ('f', Color::White),
-            ('y', Color::White),
-            ('m', Color::White),
-            ('w', Color::White),
-            ('n', Color::White),
-            ('q', Color::White),
-            ('h', Color::White),
-            ('d', Color::White),
-            ('e', Color::White),
-            ('k', Color::White),
-            ('r', Color::White),
-            ('x', Color::White),
-            ('-', Color::White),
-        ]),
-    )
+fn color_map_monochrome() -> ColorMap {
+    ColorMap {
+        name: "Mono".to_string(),
+        map: ResidueColorMap::monochrome(),
+    }
 }
 
 pub fn color_map_lesk() -> ColorMap {
-    ColorMap::new(
-        "Lesk".into(),
-        HashMap::from([
-            ('G', ORANGE),
-            ('A', ORANGE),
-            ('S', ORANGE),
-            ('T', ORANGE),
-            ('C', Color::Green),
-            ('V', Color::Green),
-            ('I', Color::Green),
-            ('L', Color::Green),
-            ('P', Color::Green),
-            ('F', Color::Green),
-            ('Y', Color::Green),
-            ('M', Color::Green),
-            ('W', Color::Green),
-            ('N', Color::Magenta),
-            ('Q', Color::Magenta),
-            ('H', Color::Magenta),
-            ('D', Color::Red),
-            ('E', Color::Red),
-            ('K', Color::Blue),
-            ('R', Color::Blue),
-            ('X', Color::White),
-            ('g', ORANGE),
-            ('a', ORANGE),
-            ('s', ORANGE),
-            ('t', ORANGE),
-            ('c', Color::Green),
-            ('v', Color::Green),
-            ('i', Color::Green),
-            ('l', Color::Green),
-            ('p', Color::Green),
-            ('f', Color::Green),
-            ('y', Color::Green),
-            ('m', Color::Green),
-            ('w', Color::Green),
-            ('n', Color::Magenta),
-            ('q', Color::Magenta),
-            ('h', Color::Magenta),
-            ('d', Color::Red),
-            ('e', Color::Red),
-            ('k', Color::Blue),
-            ('r', Color::Blue),
-            ('x', Color::White),
-            ('-', Color::Gray),
-        ]),
-    )
+    ColorMap {
+        name: "Lesk".to_string(),
+        map: ResidueColorMap::aa_lesk(),
+    }
 }
 
 pub fn color_map_clustalx() -> ColorMap {
-    ColorMap::new(
-        "ClustalX".into(),
-        HashMap::from([
-            ('G', CLUSTALX_ORANGE),
-            ('A', CLUSTALX_BLUE),
-            ('S', CLUSTALX_GREEN),
-            ('T', CLUSTALX_GREEN),
-            ('C', CLUSTALX_PINK),
-            ('V', CLUSTALX_BLUE),
-            ('I', CLUSTALX_BLUE),
-            ('L', CLUSTALX_BLUE),
-            ('P', CLUSTALX_YELLOW),
-            ('F', CLUSTALX_BLUE),
-            ('Y', CLUSTALX_CYAN),
-            ('M', CLUSTALX_BLUE),
-            ('W', CLUSTALX_BLUE),
-            ('N', CLUSTALX_GREEN),
-            ('Q', CLUSTALX_GREEN),
-            ('H', CLUSTALX_CYAN),
-            ('D', CLUSTALX_MAGENTA),
-            ('E', CLUSTALX_MAGENTA),
-            ('K', CLUSTALX_RED),
-            ('R', CLUSTALX_RED),
-            ('X', Color::White),
-            ('g', CLUSTALX_ORANGE),
-            ('a', CLUSTALX_BLUE),
-            ('s', CLUSTALX_GREEN),
-            ('t', CLUSTALX_GREEN),
-            ('c', CLUSTALX_PINK),
-            ('v', CLUSTALX_BLUE),
-            ('i', CLUSTALX_BLUE),
-            ('l', CLUSTALX_BLUE),
-            ('p', CLUSTALX_YELLOW),
-            ('f', CLUSTALX_BLUE),
-            ('y', CLUSTALX_CYAN),
-            ('m', CLUSTALX_BLUE),
-            ('w', CLUSTALX_BLUE),
-            ('n', CLUSTALX_GREEN),
-            ('q', CLUSTALX_GREEN),
-            ('h', CLUSTALX_CYAN),
-            ('d', CLUSTALX_MAGENTA),
-            ('e', CLUSTALX_MAGENTA),
-            ('k', CLUSTALX_RED),
-            ('r', CLUSTALX_RED),
-            ('x', Color::White),
-            ('-', Color::Gray),
-        ]),
-    )
+    ColorMap {
+        name: "ClustalX".to_string(),
+        map: ResidueColorMap::aa_clustalx(),
+    }
 }
 
 pub fn color_map_jalview_nt() -> ColorMap {
-    ColorMap::new(
-        "JalView-nt".into(),
-        HashMap::from([
-            ('A', JALVIEW_NUCLEOTIDE_A),
-            ('C', JALVIEW_NUCLEOTIDE_C),
-            ('G', JALVIEW_NUCLEOTIDE_G),
-            ('T', JALVIEW_NUCLEOTIDE_T),
-            ('U', JALVIEW_NUCLEOTIDE_U),
-            ('I', JALVIEW_NUCLEOTIDE_I),
-            ('X', JALVIEW_NUCLEOTIDE_X),
-            ('R', JALVIEW_NUCLEOTIDE_R),
-            ('Y', JALVIEW_NUCLEOTIDE_Y),
-            ('W', JALVIEW_NUCLEOTIDE_W),
-            ('S', JALVIEW_NUCLEOTIDE_S),
-            ('M', JALVIEW_NUCLEOTIDE_M),
-            ('K', JALVIEW_NUCLEOTIDE_K),
-            ('B', JALVIEW_NUCLEOTIDE_B),
-            ('H', JALVIEW_NUCLEOTIDE_H),
-            ('D', JALVIEW_NUCLEOTIDE_D),
-            ('V', JALVIEW_NUCLEOTIDE_V),
-            ('N', JALVIEW_NUCLEOTIDE_N),
-            ('a', JALVIEW_NUCLEOTIDE_A),
-            ('c', JALVIEW_NUCLEOTIDE_C),
-            ('g', JALVIEW_NUCLEOTIDE_G),
-            ('t', JALVIEW_NUCLEOTIDE_T),
-            ('u', JALVIEW_NUCLEOTIDE_U),
-            ('i', JALVIEW_NUCLEOTIDE_I),
-            ('x', JALVIEW_NUCLEOTIDE_X),
-            ('r', JALVIEW_NUCLEOTIDE_R),
-            ('y', JALVIEW_NUCLEOTIDE_Y),
-            ('w', JALVIEW_NUCLEOTIDE_W),
-            ('s', JALVIEW_NUCLEOTIDE_S),
-            ('m', JALVIEW_NUCLEOTIDE_M),
-            ('k', JALVIEW_NUCLEOTIDE_K),
-            ('b', JALVIEW_NUCLEOTIDE_B),
-            ('h', JALVIEW_NUCLEOTIDE_H),
-            ('d', JALVIEW_NUCLEOTIDE_D),
-            ('v', JALVIEW_NUCLEOTIDE_V),
-            ('n', JALVIEW_NUCLEOTIDE_N),
-            ('-', Color::Gray),
-        ]),
-    )
+    ColorMap {
+        name: "JalView-nt".to_string(),
+        map: ResidueColorMap::dna_jalview(),
+    }
 }
 
 pub fn colormap_gecos(path: &str) -> Result<ColorMap, TermalError> {
@@ -276,22 +92,22 @@ pub fn colormap_gecos(path: &str) -> Result<ColorMap, TermalError> {
     let cm: serde_json::Value = serde_json::from_reader(reader).unwrap();
 
     //println!("{:#?}", cm["colors"]);
-    let mut color_map: HashMap<char, Color> = HashMap::new();
+    let mut color_map = ResidueColorMap::with_default(GAP_COLOR, RGB_WHITE);
     let orig_map = &cm["colors"];
     if let Object(map) = orig_map {
         //println!("Found map: {:#?}", map);
         for (k, v) in map {
             let color_str = serde_json::from_value::<String>(v.clone()).unwrap();
             let hex_color = HexColor::parse_rgb(&color_str).unwrap();
-            let color = Color::Rgb(hex_color.r, hex_color.g, hex_color.b);
-            let residue = k.chars().collect::<Vec<char>>()[0];
-            let residue_lc = residue.to_ascii_lowercase();
-            color_map.insert(residue, color);
-            color_map.insert(residue_lc, color);
+            let color = Rgb { r: hex_color.r, g: hex_color.g, b: hex_color.b };
+            let residue = k.chars().collect::<Vec<char>>()[0] as u8;
+            color_map.set_pair(residue, color);
             //println!("{} -> {}", k.chars().collect::<Vec<char>>()[0], color);
         }
-        color_map.insert('-', Color::Gray);
     }
 
-    Ok(ColorMap::new("custom".into(), color_map))
+    Ok(ColorMap {
+        name: "custom".to_string(),
+        map: color_map,
+    })
 }
